970bd4820cfde52fb79c735f4d1e4bcb975fa245
ByungwoongAn <gomster96@gmail.com>
2022-01-11 21:29:26 +0900

    feat : jsp 환경설정

---
+++ src/main/java/member/Student.java
@@ -0,0 +1,15 @@
+package member;
+
+public class Student{
+	
+	String name;
+	int number;
+	int age;
+	
+	
+	public Student(String name, int number, int age) {
+		this.name = name;
+		this.number = number;
+		this.age = age;
+	}
+}
